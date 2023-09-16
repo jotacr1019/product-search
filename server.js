5dca14e5db94cbee9778ac0403c6d3a95392cbeb
Jose Fernandez <jotaj1019@gmail.com>
2023-09-15 18:27:32 -0600

    commit: fixing the path route to index.html

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 const history = require("connect-history-api-fallback");
 
 const app = express();
 
 app.use(history());
 
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+app.get("*", function(req, res) {
+    res.status(200).sendFile(path.resolve(__dirname, "../../dist/index.html"));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
